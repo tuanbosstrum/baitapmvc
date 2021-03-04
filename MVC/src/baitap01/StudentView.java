aa92aa60d628fca42ada548fbf1ecf1ac8a8c846
tuan <tuanbosstrum@gmail.com>
2021-03-04 13:33:03 +0700

    baitap01

--- MVC/src/baitap01/StudentView.java
+++ MVC/src/baitap01/StudentView.java
@@ -38,36 +38,57 @@ public class StudentView {
         return sv;
     }
 
-    public ArrayList<Student> nhap_du_lieu_nhieu_sinh_vien() {
+    public ArrayList<Student> nhap_du_lieu_nhieu_sinh_vien(){
         ArrayList<Student> result = new ArrayList<>();
         boolean flag = true;
-        while (flag) {
+        while(flag){
             System.out.println("Ban co muon nhap du lieu khong? (y/n)");
             Scanner sc = new Scanner(System.in);
             String answer = sc.nextLine();
-            if ("n".equals(answer) || "N".equals(answer)) {
+            if ("n".equals(answer) || "N".equals(answer)){
                 flag = false;
-            } else {
+            }else{
                 Student sv = them_du_lieu_sinh_vien();
                 result.add(sv);
             }
         }
         return result;
     }
-
-    public void hien_thi_danh_sach_sinh_vien(ArrayList<Student> students) {
-
+    
+    public void hien_thi_danh_sach_sinh_vien(ArrayList<Student> students){
+        
         System.out.println("++++++++++++++++++++++++++++++++++");
-        for (Student sv : students) {
+        for(Student sv: students){
             System.out.println("++++ " + sv.toString() + " ++++");
-        }
+        }    
         System.out.println("++++++++++++++++++++++++++++++++++");
     }
-
+    
     //Tra ve so thu tu dau tien cua sv trong mang tim kiem dc
-    public int tim_sv_theo_sdt(ArrayList<Student> students) {
+    private int tim_sv_theo_sdt(ArrayList<Student> students, String sdt){
         int result = -1; //khong tim thay
-
+        int i = 0;
+        for(Student sv: students){
+            if (sv.getPhone().contains(sdt)){
+                result = i;
+                break;
+            }
+            i += 1;    
+        }
         return result;
+    }
+    
+    public void tim_kiem_sdt(ArrayList<Student> students){
+        System.out.println("+++++++++++++++++++++++");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so dien thoai can tim kiem: ");
+        String sdt = sc.nextLine();
+        int kq = tim_sv_theo_sdt(students, sdt);
+        if (kq == -1){
+            System.out.println("Khong tim thay");
+        }else{
+            System.out.println("Phan tu thu " + (kq + 1));
+        }
+        System.out.println("+++++++++++++++++++++++");
     }
 }
