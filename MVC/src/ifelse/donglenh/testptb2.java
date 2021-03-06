2f1eba13c03c534714db4e8819f0b826a7e7c8c8
tuan <tuanbosstrum@gmail.com>
2021-03-06 13:32:23 +0700

    ifelse.donglenh

---
+++ MVC/src/ifelse/donglenh/testptb2.java
@@ -0,0 +1,15 @@
+
+package ifelse.donglenh;
+
+import ifelse.logiclayer.ptb2;
+
+public class testptb2 {
+
+    public static void main(String[] args) {
+        System.out.println("Gia pt bac 2");
+        ptb2 obj = new  ptb2(2, -4, 2);
+        obj.giaipt();
+        System.out.println(obj.getKetqua());
+    }
+    
+}
