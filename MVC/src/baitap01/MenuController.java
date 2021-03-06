98cd108be90e4d7988d11b3705bf16855b6c9512
tuan <tuanbosstrum@gmail.com>
2021-03-06 14:17:19 +0700

    baitap01

--- MVC/src/baitap01/MenuController.java
+++ MVC/src/baitap01/MenuController.java
@@ -10,7 +10,8 @@ package baitap01;
  * @author HUY TUAN
  */
 public class MenuController {
-private StudentController st_contrl;
+
+    private StudentController st_contrl;
     private MenuView mnview;
 
     public MenuController() {
@@ -24,19 +25,22 @@ private StudentController st_contrl;
         this.st_contrl = st_contrl;
         this.mnview = mnview;
     }
-    
-    
-    
-    public void lua_chon_menu(){
-        int kq = mnview.hien_thi_menu();
-        switch(kq){
-            case 1: 
-                st_contrl.nhap_danh_sach_sinh_vien();
-                break;
-            case 2:
-                st_contrl.tim_kiem_sv_theo_sdt();
-                break;
-                
+
+    public void lua_chon_menu() {
+        boolean flag = true;
+        while (flag) {
+            int kq = mnview.hien_thi_menu();
+            switch (kq) {
+                case 1:
+                    st_contrl.nhap_danh_sach_sinh_vien();
+                    break;
+                case 2:
+                    st_contrl.tim_kiem_sv_theo_sdt();
+                    break;
+                case 3:
+                    flag = false;
+                    break;
+            }
         }
-    }   
+    }
 }
